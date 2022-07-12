d15312702491bb130739e3e4a272e7fd221b8479
Walt Lungan <waltlungan@gmail.com>
2022-07-12 11:42:40 -0700

    added public js file for frontent

---
+++ public/main.js
@@ -0,0 +1,7 @@
+console.log('We are connected')
+
+const deleteExercise = document.querySelector('bi-x-circle')
+
+let exercise = document.querySelector('exercise')
+
+console.log(Array.from(exercise))
