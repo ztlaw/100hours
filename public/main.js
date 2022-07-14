b993fbc063cbdf39c55b6cb3a89112574568aeac
Walt Lungan <waltlungan@gmail.com>
2022-07-13 22:47:04 -0700

    issue: element refreshes on x click

--- public/main.js
+++ public/main.js
@@ -10,6 +10,27 @@ async function exerciseDelete(){
     const exerciseName = this.parentNode
     console.log(exerciseName)
     if (exerciseName){
+       
+       
+       
+        try{
+            const response = await fetch('deleteExercise', {
+                method: 'delete',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                  'exerciseS': exerciseName
+                })
+              })
+            const data = await response.json()
+            console.log(data)
+            location.reload()
+    
+        }catch(err){
+            console.log(err)
+        }
+    }
+
+
        exerciseName.parentNode.style.visibility = 'hidden'
     }
-}
+
