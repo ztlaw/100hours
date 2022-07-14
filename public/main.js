d1885dc1f1d462ad496ae5f96fe4e0aaff5b18b3
Walt Lungan <waltlungan@gmail.com>
2022-07-13 21:53:50 -0700

    visibility changes

--- public/main.js
+++ public/main.js
@@ -3,16 +3,13 @@ const exerciseContainer = document.querySelectorAll('.container')
 const deleteExercise = document.querySelectorAll('.bi-x-circle')
 
 Array.from(deleteExercise).forEach( element => {
-
     element.addEventListener('click', exerciseDelete)
 })
 
 async function exerciseDelete(){
-    console.log('click successful')
-    console.log(exerciseContainer)
     const exerciseName = this.parentNode
     console.log(exerciseName)
     if (exerciseName){
-       exerciseName.style.visibility = 'hidden'
+       exerciseName.parentNode.style.visibility = 'hidden'
     }
 }
