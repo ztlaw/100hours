cf49d7dd7913280f5245ff74c8a7d550f255cf83
Walt Lungan <waltlungan@gmail.com>
2022-07-13 21:45:45 -0700

    front end visbility testing

--- public/main.js
+++ public/main.js
@@ -3,9 +3,16 @@ const exerciseContainer = document.querySelectorAll('.container')
 const deleteExercise = document.querySelectorAll('.bi-x-circle')
 
 Array.from(deleteExercise).forEach( element => {
+
     element.addEventListener('click', exerciseDelete)
 })
 
 async function exerciseDelete(){
-    
+    console.log('click successful')
+    console.log(exerciseContainer)
+    const exerciseName = this.parentNode
+    console.log(exerciseName)
+    if (exerciseName){
+       exerciseName.style.visibility = 'hidden'
+    }
 }
