6bfb5da0f60c2c1e0ddbb38ffa9cb82955eab969
Walt Lungan <waltlungan@gmail.com>
2022-07-15 00:00:18 -0700

    adding comments

--- public/main.js
+++ public/main.js
@@ -1,20 +1,20 @@
 console.log('We are connected')
-const exerciseContainer = document.querySelectorAll('.container')
-const deleteExercise = document.querySelectorAll('.bi-x-circle')
+const exerciseContainer = document.querySelectorAll('.container') // selecting all .container class elements
+const deleteExercise = document.querySelectorAll('.bi-x-circle') // selecting all X image elements
 
 Array.from(deleteExercise).forEach( element => {
-    element.addEventListener('click', exerciseDelete)
+    element.addEventListener('click', exerciseDelete) // adding click event to each element
 })
 
 async function exerciseDelete(){
-    const exerciseName = this.parentNode.childNodes[1].innerText
+    const exerciseName = this.parentNode.childNodes[1].innerText // looking into the html element object and finding its text, which would be the exercise name
     console.log(exerciseName)
   //  if (exerciseName){
        
        
        
         try{
-            const response = await fetch('deleteExercise', {
+            const response = await fetch('deleteExercise', { // awaiting data to come from deleteExercise ping
                 method: 'delete',
                 headers: {'Content-Type': 'application/json'},
                 body: JSON.stringify({
