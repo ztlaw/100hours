69efd3848d33a91cfc5feec4f9170ca8790dd459
Walt Lungan <waltlungan@gmail.com>
2022-07-20 23:28:15 -0700

    adding complete exercise function

--- public/main.js
+++ public/main.js
@@ -28,12 +28,31 @@ async function exerciseDelete(){
         }
       }
 
+ //what do i want to add here?
+  // after clicking the complete exercise, each exercise will turn from neutral color to green, and it gets placed into local storage? 
+  //we will be able to find the old workouts with the click of a button coming from local storage. 
+  //need to find a better way to archive completed exercises
 Array.from(completeExercise).forEach( element => {
   element.addEventListener('click', exerciseDone) // adding click event to each element
   })
 
-  async function exerciseDone(){
+  async function exerciseDone() {
     this.parentNode.parentNode.style.background = 'green'
+    try{
+      const response = await fetch('completedExercise', { // awaiting data to come from deleteExercise ping
+          method: 'update',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            'exerciseS': exerciseName ///need to fix this. need to use clickevent to move object into a different collection. 
+          })
+        })
+      const data = await response.json()
+      console.log(data)
+     location.reload()
+
+  }catch(err){
+      console.log(err)
+  }
   }
 
   //what do i want to add here?
